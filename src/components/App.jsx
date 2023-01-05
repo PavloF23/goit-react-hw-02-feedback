8466791a46ba3443af807bf088f75014ed527817
PavloF23 <107143084+PavloF23@users.noreply.github.com>
2023-01-05 19:10:38 +0200

    up05012023

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,39 +1,64 @@
-import { Component } from "react"
-// import { Feedbacks } from "./Feedbacks/Feedbacks";
+import { Component } from 'react';
+import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
+import { Statistics } from './Statistics/Statistics';
+import { Section } from './Section/Section';
+import { AppStyle } from './App.styled';
+import {FeedbackList} from './FeedbackList.styled';
+import { Notification } from './Notification/Notification';
 
 export class App extends Component {
-
   state = {
     good: 0,
     neutral: 0,
-    bad: 0
+    bad: 0,
   };
 
-  // leaveFeedback = option => {
-  //   this.setState(prevState => ({
-  //     [option]: prevState[option] + 1,
-  //   }));
-  // };
+  //функція що піддраховує певний отзив;
+  leaveFeedback = option => {
+    this.setState(prevState => ({
+      [option]: prevState[option] + 1,
+    }));
+  };
 
-  // countTotalFeedback = () => {
-  //   return this.state.good + this.state.neutral + this.state.bad;
-  // };
+  //функція що піддраховує кількість отзивів;
+  countTotalFeedback = () => {
+    return this.state.good + this.state.neutral + this.state.bad;
+  };
+
+  //функція що підраховує відсоток позитивних отзивів;
+  countPositiveFeedbackPercentage = () => {
+    if (!this.countTotalFeedback()) {
+      return 0;
+    }
+    return Math.round((this.state.good / this.countTotalFeedback()) * 100);
+  };
 
   render() {
-  return (
-    <>
-      <h1>Please leave feetback</h1>
-      {/* <Feedbacks /> */}
-      <div>
-        <button type="button" onClick={() => (console.log('good +1'))}>Good</button>
-        <button type="button" onClick={() => (console.log('neutral +1'))}>Neutral</button>
-        <button type="button" onClick={() => (console.log('bad +1'))}>Bad</button>
-      </div>
-      <h2>Statistics</h2>
-      <p>Good:<span>{this.state.good}</span></p>
-      <p>Neutral:<span>{this.state.neutral}</span></p>
-      <p>bad:<span>{this.state.bad}</span></p>
-
-    </>
-  );
-}};
+    return (
+      <AppStyle>
+        
+         <FeedbackList>
+         <Section title="Please leave feedback">
+            <FeedbackOptions 
+              options={Object.keys(this.state)}
+              onLeaveFeedback={this.leaveFeedback}
+            />
+          </Section>
+          <Section title="Statistics">
+          {this.countTotalFeedback() ? (
+            <Statistics
+              good={this.state.good}
+              neutral={this.state.neutral}
+              bad={this.state.bad}
+              total={this.countTotalFeedback()}
+              positivePercentage={this.countPositiveFeedbackPercentage()}
+            />
+          ) : (
+            <Notification message="There is no feedback"></Notification>
+          )}
+          </Section>
+        </FeedbackList>
+      </AppStyle>
+    );
+  }
+}
